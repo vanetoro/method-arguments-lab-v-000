9465dc96eb6e36be2a6087f0a924b0c368010d8d
Vanessa Toro <vanetoro84@gmail.com>
2018-03-20 18:01:03 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
   
 end
+
+def introdction_with_language(name, lang)
+  puts "Hi, my name is #{name} and I am learning to program in #{lang}."
+end
